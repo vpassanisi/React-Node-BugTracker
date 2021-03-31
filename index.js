c02ca7d3ec65bd96f1bf2e6ae8a809735b1daabd
vpassanisi <vpassanisi@gmail.com>
2021-03-30 17:00:20 -0700

    initial electron commit

--- index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-require("colors");
-const server = require("./server");
-
-const port = process.env.PORT || 5500;
-
-server.listen(port, () =>
-  console.log(`Server is running on ${port}`.yellow.bold)
-);
--- routes/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = router => {
-  router.prefix("/api/v1");
-  router.use("/auth", require("./auth"));
-  router.use("/bugs", require("./bugs"));
-  router.use("/projects", require("./projects"));
-};
